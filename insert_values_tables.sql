7911f8dbfc432768c2ca976eda116c9c1850b2dd
Jose Ignacio <128377342+joseilegaz96@users.noreply.github.com>
2023-03-28 14:05:48 +0200

    Add files via upload

---
+++ insert_values_tables.sql
@@ -0,0 +1,11 @@
+INSERT INTO `canciones` (`id`, `nombre_cancion`, `duracion`, `genero`, `artista`, `ruta`, `nombre_fichero`, `Album`) VALUES
+(1, 'Heroes', '00:05:04', 'Glam Rock', 2, 'Musik', 'helden.mp3', 'Heroes'),
+(2, 'Lola', '00:04:47', 'Rock', 4, 'Musik', 'lola.mp3', 'One For the Road'),
+(3, 'In the jungle', '00:01:00', 'PoP', 3, 'Musik', 'one_minute.mp3', 'AA');
+
+
+INSERT INTO `artista` (`id`, `artista`) VALUES
+(1, 'Pink Floyd'),
+(2, 'David Bowie'),
+(3, 'John Wasson'),
+(4, 'The Kings');
